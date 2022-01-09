a93eaad8d9a4ce8340fdb880624c04551e76aa02
Tony Bowden <tony@tmtm.com>
2022-01-09 05:29:47 +0200

    compare ruwiki Youth+Sports Ministers

---
+++ sources/youth-sports-enwiki/scraper.rb
@@ -0,0 +1,30 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+require 'open-uri/cached'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Number'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[ordinal name start end].freeze
+    end
+
+    def empty?
+      raw_end.include?('?') || raw_start.include?('?') || raw_start.include?('c. ')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
